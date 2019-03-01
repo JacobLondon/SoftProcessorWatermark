b8a10e38a98fe3b0e642392845d4561b56edc456
Jacob <jelondon12@gmail.com>
2019-02-28 17:23:32 -0800

    made a version in C++

---
+++ High Level Implementations/CWatermark/CWatermark/main.cpp
@@ -0,0 +1,77 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+// must be using C++17
+// 64-bit, release to run
+int main(int argc, char** argv)
+{
+	// open original and watermark with rgba
+	std::string opath = fs::current_path().string() + "\\images\\image2.png";
+	std::string wpath = fs::current_path().string() + "\\images\\watermark2.png";
+	std::string outpath = fs::current_path().string() + "\\images\\output.png";
+	//std::cout << opath << std::endl;
+	//bool exists = std::filesystem::exists(path);
+	//std::cout << exists << std::endl;
+
+    // open original and watermark with rgba
+	cv::Mat original = cv::imread(opath, cv::IMREAD_UNCHANGED);
+	cv::Mat watermark = cv::imread(wpath, cv::IMREAD_UNCHANGED);
+	// copy the original to not modify it for the output
+	cv::Mat copy = original;
+
+	// check for invalid image input
+	if (original.empty() || watermark.empty())
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	// make sure the images are the same size
+	assert(original.size() == watermark.size());
+
+	int cols, rows;
+	cols = original.cols;
+	rows = original.rows;
+	int vals = 4;	// 4 values, rgba
+
+	cv::Vec4b oRGBA, wRGBA, cRGBA;
+
+	// loop over the original pixel by pixel
+	for (int i = 0; i < cols; i++)
+		for (int j = 0; j < rows; j++)
+		{
+			// get rgba to check if the watermark has alpha+
+			wRGBA = watermark.at<cv::Vec4b>(cv::Point(i, j));
+
+			if (wRGBA[vals - 1] != 0)
+			{
+				// get rgb of original pixel so it can be averaged
+				oRGBA = original.at<cv::Vec4b>(cv::Point(i, j));
+				cRGBA = copy.at<cv::Vec4b>(cv::Point(i, j));
+
+				// modify the rgba
+				for (int k = 0; k < vals; k++)
+					cRGBA[k] = (oRGBA[k] + wRGBA[k]) / 2;
+
+				copy.at<cv::Vec4b>(cv::Point(i, j)) = cRGBA;
+			}
+		}
+		
+	
+	// save the output
+	cv::imwrite(outpath, copy);
+
+	// Create a window and display image
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+    imshow("Display window", copy);
+	cv::waitKey(0);
+
+	cv::destroyAllWindows();
+
+    return 0;
+}
